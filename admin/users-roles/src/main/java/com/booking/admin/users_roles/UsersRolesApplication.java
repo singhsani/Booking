6e5d3c99b86d5a8b636ab092d7d6ce562d577064
includestdioh <pratapsinghh03@gmail.com>
2025-05-31 18:22:58 +0530

    Third

---
+++ admin/users-roles/src/main/java/com/booking/admin/users_roles/UsersRolesApplication.java
@@ -0,0 +1,13 @@
+package com.booking.admin.users_roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRolesApplication.class, args);
+	}
+
+}
